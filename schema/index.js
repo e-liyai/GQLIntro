66d21be16b39b044390dfdfc5cd25834c5cdc94c
Andela-eugene <eugene.liyai@andela.com>
2019-02-06 21:18:15 +0300

    [feat: #] add custom graphql types

--- schema/index.js
+++ schema/index.js
@@ -2,15 +2,22 @@ const {
 	GraphQLSchema,
 	GraphQLObjectType,
 	GraphQLString,
+	GraphQLNonNull,
 } = require('graphql')
+const MeType = require('./types/me')
 
 const RootQueryType = new GraphQLObjectType({
 	name: 'RootQueryType',
 	fields: {
-		hello: {
-			type: GraphQLString,
-			description: 'The *mandatory* hello world example, GraphQL style',
-			resolve: () => 'world'
+		me: {
+			type: MeType,
+			description: 'The current user identified by an api key',
+			args: {
+				key: { type: new GraphQLNonNull(GraphQLString) }
+			},
+			resolve: () => {
+				// read user info from database
+			}
 		}
 	}
 })
---
+++ schema/types/me.js
@@ -0,0 +1,14 @@
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLID,
+	GraphQLNonNull,
+} = require('graphql')
+
+module.exports = new GraphQLObjectType({
+	name: 'MeType',
+	fields: {
+		id: { type: GraphQLID },
+		email: { type: new GraphQLNonNull(GraphQLString) }
+	}
+})
